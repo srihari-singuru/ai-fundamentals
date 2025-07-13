6e332f8de01c5cf3d2e995965bcfa73dcd1c8b5e
Srihari Singuru <srihari.singuru@gmail.com>
2025-07-13 17:33:57 +0530

    ChatCompletionController with basic exception handling

--- src/main/java/com/srihari/ai/controller/ChatCompletionController.java
+++ src/main/java/com/srihari/ai/controller/ChatCompletionController.java
@@ -1,11 +1,15 @@
 package com.srihari.ai.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
 @RestController
+@RequestMapping("/v1")
+@Slf4j
 public class ChatCompletionController {
     private final ChatClient chatClient;
 
@@ -13,11 +17,17 @@ public class ChatCompletionController {
         this.chatClient = chatClientBuilder.build();
     }
 
-    @GetMapping("/demo")
+    @GetMapping(value = "/ai-chat")     // produces = "text/event-stream" for SSE
     public Flux<String> getResponse() {
-        String promptMessage = "Write a small bedtime story about a unicorn";
+        String promptMessage = "Explain the concept of AI in simple terms.";
         return chatClient.prompt(promptMessage)
                 .stream()
-                .content();
+                .content()
+                .onErrorResume(ex -> Flux.just("Exception: " + extractErrorMessage(ex)));
+    }
+
+    private String extractErrorMessage(Throwable ex) {
+        log.error("Error occurred while processing chat completion request", ex);
+        return ex.getMessage();
     }
 }
